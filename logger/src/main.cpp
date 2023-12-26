e03a7c605a93bd318f160c7d2e92cd667ce80568
LVOrac <joecheong2006@gmail.com>
2023-12-27 00:23:28 +0800

    logger demo

--- logger/src/main.cpp
+++ logger/src/main.cpp
@@ -1,68 +1,100 @@
-#include <iostream>
+#include <stdio.h>
 #include <string>
-#include <tuple>
-#include <vector>
+
+// Pattern '{1}, {0}'  Args
+// <------ Formatter ------>
+// Format '%f, %s'    Args
 
 namespace Log {
+    template <typename T>
     struct Pattern {
-        Pattern(const std::string& ptn)
-            : pattern(ptn)
-        {}
-
-        void operator=(const std::string& ptn) {
-            pattern = ptn;
-        }
-
-        std::string pattern;
+        explicit Pattern() {}
+        static void Log(const T& value) { (void)value; }
     };
 
-    template <typename... Args>
-    struct Format {
-        explicit Format(const std::string& ptn)
-            : ptn(ptn)
-        {}
+#define __DEFINE_PATTERN_BASIC_LOG(type, pattern, ...)\
+        template <>\
+        struct Pattern<type> {\
+            static void Log(const type& value) {\
+                printf(pattern, __VA_ARGS__);\
+            }\
+        };\
 
-        Pattern ptn;
-        std::vector<void*> parameter;
-    };
+__DEFINE_PATTERN_BASIC_LOG(int, "%d", value);
+__DEFINE_PATTERN_BASIC_LOG(float, "%g", value);
+__DEFINE_PATTERN_BASIC_LOG(double, "%lg", value);
+__DEFINE_PATTERN_BASIC_LOG(char*, "%s", value);
+__DEFINE_PATTERN_BASIC_LOG(char, "%c", value);
+__DEFINE_PATTERN_BASIC_LOG(std::string, "%s", value.c_str());
 
-    template <typename T>
-    struct Formatter {
-        Format<int> format(T value) {
-            return Format<int>("", value);
+    template <typename Arg>
+    void info(const std::string& pattern, const Arg& arg) {
+        std::string& ptn = const_cast<std::string&>(pattern);
+        auto fristIndex = ptn.find("{");
+        auto endIndex = ptn.find("}");
+        for (size_t i = 0; i <= endIndex; ++i)  {
+            if (i < fristIndex) {
+                Pattern<char>::Log(ptn[0]);
+                ptn.erase(0, 1);
+                continue;
+            }
+            ptn.erase(0, 1);
         }
+        Pattern<Arg>::Log(arg);
+    }
 
-        void print_parameter() {
+    template <typename Arg, typename... Args>
+    void info(const std::string& pattern, const Arg& arg, const Args& ...args) {
+        std::string& ptn = const_cast<std::string&>(pattern);
+        auto fristIndex = ptn.find("{");
+        auto endIndex = ptn.find("}");
+        for (size_t i = 0; i <= endIndex; ++i)  {
+            if (i < fristIndex) {
+                Pattern<char>::Log(ptn[0]);
+                ptn.erase(0, 1);
+                continue;
+            }
+            ptn.erase(0, 1);
         }
-    };
-
-    template <typename T>
-    void Demo(T value) {
-        Formatter<T> fmt;
-        std::cout << fmt.format(value).ptn.pattern;
+        Pattern<Arg>::Log(arg);
+        info(ptn, args...);
     }
 }
 
-struct vec2 {
-    vec2(int x, int y)
-        : x(x), y(y)
+#define DEFINE_PATTERN_BASIC_LOG(type, pattern, ...)\
+        template <>\
+        struct Log::Pattern<type> {\
+            static void Log(const type& value) {\
+                printf(pattern, __VA_ARGS__);\
+            }\
+        };\
+
+#define DEFINE_PATTERN_BASIC_LOGLN(type, pattern, ...)\
+    DEFINE_PATTERN_BASIC_LOG(type, pattern "\n", __VA_ARGS__);\
+
+template <typename T>
+struct vec3 {
+    friend struct Log::Pattern<vec3<float>>;
+    vec3()
     {}
 
-    friend Log::Formatter<vec2>;
+    vec3(T x, T y, T z)
+        : x(x), y(y), z(z)
+    {}
+
 private:
-    int x, y;
+    T x, y, z;
+
 };
 
-template <>
-struct Log::Formatter<vec2> {
-    Format<int, int> format(vec2 v) {
-        Format<int, int> format("vec2[{}, {}]");
-        return format;
-    }
-};
+DEFINE_PATTERN_BASIC_LOG(vec3<float>, "[%.3g, %.3g, %.3g]", value.x, value.y, value.z)
+
+#define LOG_INFO(pattern, ...)\
+    Log::info(pattern, __VA_ARGS__);
 
 int main() {
-    //print_parameter<int, float>(vparameter);
-    //auto vec = parameter<int>(1, 2, 3);
-    //Log::Demo<vec2>(vec2(1, 2));
+    vec3<float> v(6, 3, 9);
+    vec3<float> v1(1, 2, 3);
+    LOG_INFO("v:{} v1:{} int:{} double:{} float:{}", v, v1, 10, 2.405, 4.3f);
+    return 0;
 }
