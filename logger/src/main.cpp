2bedbae7dcb40fdaee405c1d4eba06b02765ec0f
LVOrac <joecheong2006@gmail.com>
2023-12-28 00:04:37 +0800

    add basic color pattern

--- logger/src/main.cpp
+++ logger/src/main.cpp
@@ -27,37 +27,68 @@ __DEFINE_PATTERN_BASIC_LOG(char*, "%s", value);
 __DEFINE_PATTERN_BASIC_LOG(char, "%c", value);
 __DEFINE_PATTERN_BASIC_LOG(std::string, "%s", value.c_str());
 
+    enum ColorPattern : unsigned char {
+        Reset = 0,
+        Black = 30,
+        Red,
+        Green,
+        Yellow,
+        Blue,
+        Magenta,
+        Cyan,
+        White,
+    };
+
+    void SetColor(unsigned char colorPattern) { 
+        std::string pattern = "\033[1;" + std::to_string(colorPattern) + "m";
+        Pattern<std::string>::Log(pattern); 
+    }
+
+    void SetColor(ColorPattern colorPattern = Reset) { 
+        SetColor((unsigned char)colorPattern);
+    }
+
+    const std::string find_string_between(const std::string& str, char a, char b) {
+        std::string result;
+        auto fristIndex = str.find(a);
+        auto endIndex = str.find(b);
+        for (size_t i = fristIndex + 1; i < endIndex; ++i)
+            result += str[i];
+        return result;
+    }
+
     template <typename Arg>
-    void info(const std::string& pattern, const Arg& arg) {
+    void Info(const std::string& pattern, const Arg& arg) {
         std::string& ptn = const_cast<std::string&>(pattern);
-        auto fristIndex = ptn.find("{");
-        auto endIndex = ptn.find("}");
-        for (size_t i = 0; i <= endIndex; ++i)  {
-            if (i < fristIndex) {
-                Pattern<char>::Log(ptn[0]);
-                ptn.erase(0, 1);
-                continue;
-            }
+        auto bstart = ptn.find("{");
+        for (unsigned long long i = 0; i < bstart; ++i) {
+            Pattern<char>::Log(ptn[0]);
             ptn.erase(0, 1);
         }
+        std::string bs = find_string_between(pattern, '{', '}');
+        ptn.erase(0, bs.length() + 2);
+
         Pattern<Arg>::Log(arg);
+        Pattern<std::string>::Log(ptn);
     }
 
     template <typename Arg, typename... Args>
-    void info(const std::string& pattern, const Arg& arg, const Args& ...args) {
+    void Info(const std::string& pattern, const Arg& arg, const Args& ...args) {
         std::string& ptn = const_cast<std::string&>(pattern);
-        auto fristIndex = ptn.find("{");
-        auto endIndex = ptn.find("}");
-        for (size_t i = 0; i <= endIndex; ++i)  {
-            if (i < fristIndex) {
-                Pattern<char>::Log(ptn[0]);
-                ptn.erase(0, 1);
-                continue;
-            }
+        auto bstart = ptn.find("{");
+        for (unsigned long long i = 0; i < bstart; ++i) {
+            Pattern<char>::Log(ptn[0]);
             ptn.erase(0, 1);
         }
+        std::string bs = find_string_between(pattern, '{', '}');
+        ptn.erase(0, bs.length() + 2);
+
         Pattern<Arg>::Log(arg);
-        info(ptn, args...);
+        Info(pattern, args...);
+    }
+
+    void Info(const std::string& pattern) {
+        Pattern<std::string>::Log(pattern);
     }
 }
 
@@ -74,7 +105,7 @@ __DEFINE_PATTERN_BASIC_LOG(std::string, "%s", value.c_str());
 
 template <typename T>
 struct vec3 {
-    friend struct Log::Pattern<vec3<float>>;
+    friend struct Log::Pattern<vec3>;
     vec3()
     {}
 
@@ -87,14 +118,19 @@ private:
 
 };
 
-DEFINE_PATTERN_BASIC_LOG(vec3<float>, "[%.3g, %.3g, %.3g]", value.x, value.y, value.z)
+typedef vec3<float> vec3f;
+typedef vec3<int> vec3i;
 
-#define LOG_INFO(pattern, ...)\
-    Log::info(pattern, __VA_ARGS__);
+DEFINE_PATTERN_BASIC_LOG(vec3f, "[%.3g, %.3g, %.3g]", value.x, value.y, value.z)
+DEFINE_PATTERN_BASIC_LOG(vec3i, "[%d, %d, %d]", value.x, value.y, value.z)
 
 int main() {
-    vec3<float> v(6, 3, 9);
-    vec3<float> v1(1, 2, 3);
-    LOG_INFO("v:{} v1:{} int:{} double:{} float:{}", v, v1, 10, 2.405, 4.3f);
-    return 0;
+    vec3f v(6.1, 3.04, 9.889);
+    vec3i v1(1, 2, 3);
+    for (int i = 0; i < 8; ++i) {
+        Log::SetColor((unsigned char)(30 + i));
+        Log::Info("v: {} v1: {} int: {} double: {} float: {}\n", v, v1, 10, 2.405, 4.3f);
+    }
+    Log::Info("end");
+    Log::SetColor();
 }
