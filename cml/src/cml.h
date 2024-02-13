be7ec5644870cc5d603fe700e95ef4d6a9fa7c67
LVOrac <joecheong2006@gmail.com>
2024-02-13 15:25:44 +0800

    update cml

--- cml/src/cml.h
+++ cml/src/cml.h
@@ -21,34 +21,16 @@
 #define CML_INLINE static inline __attribute((always_inline))
 
 #define DEFINE_VEC3(T, suffix)\
-    typedef union vec3##suffix{\
-        struct {\
-            T x, y, z;\
-        };\
-        struct {\
-            T r, g, b;\
-        };\
-    } vec3##suffix;
+    typedef T vec3##suffix[3];\
 
 #define DEFINE_VEC4(T, suffix)\
-    typedef union {\
-        struct {\
-            T x, y, z, w;\
-        };\
-        struct {\
-            T r, g, b, a;\
-        };\
-    } vec4##suffix;
+    typedef T vec4##suffix[4];\
 
 #define DEFINE_MAT3(suffix)\
-    typedef struct {\
-        vec3##suffix col[3];\
-    } mat3##suffix;
+    typedef vec3##suffix mat3##suffix[3];
 
 #define DEFINE_MAT4(suffix)\
-    typedef struct {\
-        vec4##suffix col[4];\
-    } mat4##suffix;
+    typedef vec4##suffix mat4##suffix[4];
 
 #define SET_DEFAULT_VEC3(suffix) typedef vec3##suffix vec3
 #define SET_DEFAULT_VEC4(suffix) typedef vec4##suffix vec4
@@ -58,217 +40,245 @@
 
 #define DEFINE_BASIC_VEC3_BEHAVIOUR(T, suffix)\
     CML_INLINE\
-    void vec3##suffix##_copy(vec3##suffix* dest, vec3##suffix* src) {\
+    void vec3##suffix##_copy(vec3##suffix dest, vec3##suffix src) {\
         memcpy(dest, src, sizeof(T) * 3);\
     }\
     CML_INLINE\
-    void vec3##suffix##_cross(vec3##suffix* out, vec3##suffix* v1, vec3##suffix* v2) {\
-        out->x = v1->y * v2->z - v1->z * v2->y;\
-        out->y = v1->z * v2->x - v1->x * v2->z;\
-        out->z = v1->x * v2->y - v1->y * v2->x;\
+    void vec3##suffix##_cross(vec3##suffix out, vec3##suffix v1, vec3##suffix v2) {\
+        out[0] = v1[1] * v2[2] - v1[2] * v2[1];\
+        out[1] = v1[2] * v2[0] - v1[0] * v2[2];\
+        out[2] = v1[0] * v2[1] - v1[1] * v2[0];\
     }\
     CML_INLINE\
-    T vec3##suffix##_dot(vec3##suffix* v1, vec3##suffix* v2) {\
-        return v1->x * v2->x + v1->y * v2->y + v1->z * v2->z;\
+    T vec3##suffix##_dot(vec3##suffix v1, vec3##suffix v2) {\
+        return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];\
     }
 
 #define DEFINE_BASIC_VEC4_BEHAVIOUR(T, suffix)\
     CML_INLINE\
-    void vec4##suffix##_copy(vec4##suffix* dest, vec4##suffix* src) {\
+    void vec4##suffix##_copy(vec4##suffix dest, vec4##suffix src) {\
         memcpy(dest, src, sizeof(T) * 4);\
     }\
     CML_INLINE\
-    T vec4##suffix##_dot(vec4##suffix* v1, vec4##suffix* v2) {\
-        return v1->x * v2->x + v1->y * v2->y + v1->z * v2->z + v1->w * v2->w;\
+    T vec4##suffix##_dot(vec4##suffix v1, vec4##suffix v2) {\
+        return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2] + v1[3] * v2[3];\
     }\
 
 #define DEFINE_BASIC_MAT3_BEHAVIOUR(T, suffix)\
     CML_INLINE\
-    void mat3##suffix##_zero(mat3##suffix* m) {\
-        memset(&m->col[0], 0, sizeof(T) * 3);\
-        memset(&m->col[1], 0, sizeof(T) * 3);\
-        memset(&m->col[2], 0, sizeof(T) * 3);\
+    void mat3##suffix##_zero(mat3##suffix m) {\
+        memset(m[0], 0, sizeof(T) * 3);\
+        memset(m[1], 0, sizeof(T) * 3);\
+        memset(m[2], 0, sizeof(T) * 3);\
     }\
     CML_INLINE\
-    void mat3##suffix##_copy(mat3##suffix* dest, mat3##suffix* src) {\
-        vec3##suffix##_copy(&dest->col[0], &src->col[0]);\
-        vec3##suffix##_copy(&dest->col[1], &src->col[1]);\
-        vec3##suffix##_copy(&dest->col[2], &src->col[2]);\
+    void mat3##suffix##_copy(mat3##suffix dest, mat3##suffix src) {\
+        vec3##suffix##_copy(dest[0], src[0]);\
+        vec3##suffix##_copy(dest[1], src[1]);\
+        vec3##suffix##_copy(dest[2], src[2]);\
     }\
     CML_INLINE\
-    void mat3##suffix##_mul_vec3##suffix(vec3##suffix* out, mat3##suffix* m, vec3##suffix* v) {\
-        out->x = vec3##suffix##_dot(&m->col[0], v);\
-        out->y = vec3##suffix##_dot(&m->col[1], v);\
-        out->z = vec3##suffix##_dot(&m->col[2], v);\
+    void mat3##suffix##_mul_vec3##suffix(vec3##suffix out, mat3##suffix m, vec3##suffix v) {\
+        out[0] = vec3##suffix##_dot(m[0], v);\
+        out[1] = vec3##suffix##_dot(m[1], v);\
+        out[2] = vec3##suffix##_dot(m[2], v);\
     }\
     CML_INLINE\
-    void mat3##suffix##_mul_mat3##suffix(mat3##suffix* out, mat3##suffix* m1, mat3##suffix* m2) {\
-        vec3##suffix temp = { { m2->col[0].x,  m2->col[1].x,  m2->col[2].x } };\
-        out->col[0].x = vec3##suffix##_dot(&m1->col[0], &temp);\
-        out->col[1].x = vec3##suffix##_dot(&m1->col[1], &temp);\
-        out->col[2].x = vec3##suffix##_dot(&m1->col[2], &temp);\
-        temp = (vec3##suffix){ { m2->col[0].y,  m2->col[1].y,  m2->col[2].y } };\
-        out->col[0].y = vec3##suffix##_dot(&m1->col[0], &temp);\
-        out->col[1].y = vec3##suffix##_dot(&m1->col[1], &temp);\
-        out->col[2].y = vec3##suffix##_dot(&m1->col[2], &temp);\
-        temp = (vec3##suffix){ { m2->col[0].z,  m2->col[1].z,  m2->col[2].z } };\
-        out->col[0].z = vec3##suffix##_dot(&m1->col[0], &temp);\
-        out->col[1].z = vec3##suffix##_dot(&m1->col[1], &temp);\
-        out->col[2].z = vec3##suffix##_dot(&m1->col[2], &temp);\
+    void mat3##suffix##_mul_mat3##suffix(mat3##suffix out, mat3##suffix m1, mat3##suffix m2) {\
+        {\
+        vec3##suffix temp = { m2[0][0],  m2[1][0],  m2[2][0] };\
+        out[0][0] = vec3##suffix##_dot(m1[0], temp);\
+        out[1][0] = vec3##suffix##_dot(m1[1], temp);\
+        out[2][0] = vec3##suffix##_dot(m1[2], temp);\
+        }\
+        {\
+        vec3##suffix temp = { m2[0][1],  m2[1][1],  m2[2][1] };\
+        out[0][1] = vec3##suffix##_dot(m1[0], temp);\
+        out[1][1] = vec3##suffix##_dot(m1[1], temp);\
+        out[2][1] = vec3##suffix##_dot(m1[2], temp);\
+        }\
+        {\
+        vec3##suffix temp = { m2[0][2],  m2[1][2],  m2[2][2] };\
+        out[0][2] = vec3##suffix##_dot(m1[0], temp);\
+        out[1][2] = vec3##suffix##_dot(m1[1], temp);\
+        out[2][2] = vec3##suffix##_dot(m1[2], temp);\
+        }\
     }\
 
 #define DEFINE_BASIC_MAT4_BEHAVIOUR(T, suffix)\
     CML_INLINE\
-    void mat4##suffix##_zero(mat4##suffix* m) {\
-        memset(&m->col[0], 0, sizeof(T) * 4);\
-        memset(&m->col[1], 0, sizeof(T) * 4);\
-        memset(&m->col[2], 0, sizeof(T) * 4);\
-        memset(&m->col[3], 0, sizeof(T) * 4);\
+    void mat4##suffix##_zero(mat4##suffix m) {\
+        memset(m[0], 0, sizeof(T) * 4);\
+        memset(m[1], 0, sizeof(T) * 4);\
+        memset(m[2], 0, sizeof(T) * 4);\
+        memset(m[3], 0, sizeof(T) * 4);\
     }\
     CML_INLINE\
-    void mat4##suffix##_copy(mat4##suffix* dest, mat4##suffix* src) {\
-        vec4##suffix##_copy(&dest->col[0], &src->col[0]);\
-        vec4##suffix##_copy(&dest->col[1], &src->col[1]);\
-        vec4##suffix##_copy(&dest->col[2], &src->col[2]);\
-        vec4##suffix##_copy(&dest->col[3], &src->col[3]);\
+    void mat4##suffix##_copy(mat4##suffix dest, mat4##suffix src) {\
+        vec4##suffix##_copy(dest[0], src[0]);\
+        vec4##suffix##_copy(dest[1], src[1]);\
+        vec4##suffix##_copy(dest[2], src[2]);\
+        vec4##suffix##_copy(dest[3], src[3]);\
     }\
     CML_INLINE\
-    void mat4##suffix##_mul_vec4##suffix(vec4##suffix* out, mat4##suffix* m, vec4##suffix* v) {\
-        out->x = vec4##suffix##_dot(&m->col[0], v);\
-        out->y = vec4##suffix##_dot(&m->col[1], v);\
-        out->z = vec4##suffix##_dot(&m->col[2], v);\
-        out->w = vec4##suffix##_dot(&m->col[3], v);\
+    void mat4##suffix##_mul_vec4##suffix(vec4##suffix out, mat4##suffix m, vec4##suffix v) {\
+        out[0] = vec4##suffix##_dot(m[0], v);\
+        out[1] = vec4##suffix##_dot(m[1], v);\
+        out[2] = vec4##suffix##_dot(m[2], v);\
+        out[3] = vec4##suffix##_dot(m[3], v);\
     }\
     CML_INLINE\
-    void mat4##suffix##_mul_mat4##suffix(mat4##suffix* out, mat4##suffix* m1, mat4##suffix* m2) {\
-        vec4##suffix temp = { { m2->col[0].x,  m2->col[1].x,  m2->col[2].x, m2->col[3].x } };\
-        out->col[0].x = vec4##suffix##_dot(&m1->col[0], &temp);\
-        out->col[1].x = vec4##suffix##_dot(&m1->col[1], &temp);\
-        out->col[2].x = vec4##suffix##_dot(&m1->col[2], &temp);\
-        out->col[3].x = vec4##suffix##_dot(&m1->col[3], &temp);\
-        temp = (vec4##suffix){ { m2->col[0].y,  m2->col[1].y,  m2->col[2].y, m2->col[3].y } };\
-        out->col[0].y = vec4##suffix##_dot(&m1->col[0], &temp);\
-        out->col[1].y = vec4##suffix##_dot(&m1->col[1], &temp);\
-        out->col[2].y = vec4##suffix##_dot(&m1->col[2], &temp);\
-        out->col[3].y = vec4##suffix##_dot(&m1->col[3], &temp);\
-        temp = (vec4##suffix){ { m2->col[0].z,  m2->col[1].z,  m2->col[2].z, m2->col[3].z } };\
-        out->col[0].z = vec4##suffix##_dot(&m1->col[0], &temp);\
-        out->col[1].z = vec4##suffix##_dot(&m1->col[1], &temp);\
-        out->col[2].z = vec4##suffix##_dot(&m1->col[2], &temp);\
-        out->col[3].z = vec4##suffix##_dot(&m1->col[3], &temp);\
-        temp = (vec4##suffix){ { m1->col[0].w,  m2->col[1].w,  m2->col[2].w, m2->col[3].w } };\
-        out->col[0].w = vec4##suffix##_dot(&m1->col[0], &temp);\
-        out->col[1].w = vec4##suffix##_dot(&m1->col[1], &temp);\
-        out->col[2].w = vec4##suffix##_dot(&m1->col[2], &temp);\
-        out->col[3].w = vec4##suffix##_dot(&m1->col[3], &temp);\
+    void mat4##suffix##_mul_mat4##suffix(mat4##suffix out, mat4##suffix m1, mat4##suffix m2) {\
+        {\
+        vec4##suffix temp = { m2[0][0],  m2[1][0],  m2[2][0], m2[3][0] };\
+        out[0][0] = vec4##suffix##_dot(m1[0], temp);\
+        out[1][0] = vec4##suffix##_dot(m1[1], temp);\
+        out[2][0] = vec4##suffix##_dot(m1[2], temp);\
+        out[3][0] = vec4##suffix##_dot(m1[3], temp);\
+        }\
+        {\
+        vec4##suffix temp = { m2[0][1],  m2[1][1],  m2[2][1], m2[3][1] };\
+        out[0][1] = vec4##suffix##_dot(m1[0], temp);\
+        out[1][1] = vec4##suffix##_dot(m1[1], temp);\
+        out[2][1] = vec4##suffix##_dot(m1[2], temp);\
+        out[3][1] = vec4##suffix##_dot(m1[3], temp);\
+        }\
+        {\
+        vec4##suffix temp = { m2[0][2],  m2[1][2],  m2[2][2], m2[3][2] };\
+        out[0][2] = vec4##suffix##_dot(m1[0], temp);\
+        out[1][2] = vec4##suffix##_dot(m1[1], temp);\
+        out[2][2] = vec4##suffix##_dot(m1[2], temp);\
+        out[3][2] = vec4##suffix##_dot(m1[3], temp);\
+        }\
+        {\
+        vec4##suffix temp = { m2[0][3],  m2[1][3],  m2[2][3], m2[3][3] };\
+        out[0][3] = vec4##suffix##_dot(m1[0], temp);\
+        out[1][3] = vec4##suffix##_dot(m1[1], temp);\
+        out[2][3] = vec4##suffix##_dot(m1[2], temp);\
+        out[3][3] = vec4##suffix##_dot(m1[3], temp);\
+        }\
     }\
 
 #define DEFINE_DEFAULT_BASIC_VEC3_BEHAVIOUR(T)\
     CML_INLINE\
-    void vec3_copy(vec3* dest, vec3* src) {\
+    void vec3_copy(vec3 dest, vec3 src) {\
         memcpy(dest, src, sizeof(T) * 3);\
     }\
     CML_INLINE\
-    void vec3_cross(vec3* out, vec3* v1, vec3* v2) {\
-        out->x = v1->y * v2->z - v1->z * v2->y;\
-        out->y = v1->z * v2->x - v1->x * v2->z;\
-        out->z = v1->x * v2->y - v1->y * v2->x;\
+    void vec3_cross(vec3 out, vec3 v1, vec3 v2) {\
+        out[0] = v1[1] * v2[2] - v1[2] * v2[1];\
+        out[1] = v1[2] * v2[0] - v1[0] * v2[2];\
+        out[2] = v1[0] * v2[1] - v1[1] * v2[0];\
     }\
     CML_INLINE\
-    T vec3_dot(vec3* v1, vec3* v2) {\
-        return v1->x * v2->x + v1->y * v2->y + v1->z * v2->z;\
+    T vec3_dot(vec3 v1, vec3 v2) {\
+        return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];\
     }\
 
 #define DEFINE_DEFAULT_BASIC_VEC4_BEHAVIOUR(T)\
     CML_INLINE\
-    void vec4_copy(vec4* dest, vec4* src) {\
+    void vec4_copy(vec4 dest, vec4 src) {\
         memcpy(dest, src, sizeof(T) * 4);\
     }\
     CML_INLINE\
-    T vec4_dot(vec4* v1, vec4* v2) {\
-        return v1->x * v2->x + v1->y * v2->y + v1->z * v2->z + v1->w * v2->w;\
+    T vec4_dot(vec4 v1, vec4 v2) {\
+        return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2] + v1[3] * v2[3];\
     }\
 
 #define DEFINE_DEFAULT_BASIC_MAT3_BEHAVIOUR(T)\
     CML_INLINE\
-    void mat3_zero(mat3* m) {\
-        memset(&m->col[0], 0, sizeof(T) * 3);\
-        memset(&m->col[1], 0, sizeof(T) * 3);\
-        memset(&m->col[2], 0, sizeof(T) * 3);\
+    void mat3_zero(mat3 m) {\
+        memset(m[0], 0, sizeof(T) * 3);\
+        memset(m[1], 0, sizeof(T) * 3);\
+        memset(m[2], 0, sizeof(T) * 3);\
     }\
     CML_INLINE\
-    void mat3_copy(mat3* dest, mat3* src) {\
-        vec3_copy(&dest->col[0], &src->col[0]);\
-        vec3_copy(&dest->col[1], &src->col[1]);\
-        vec3_copy(&dest->col[2], &src->col[2]);\
+    void mat3_copy(mat3 dest, mat3 src) {\
+        vec3_copy(dest[0], src[0]);\
+        vec3_copy(dest[1], src[1]);\
+        vec3_copy(dest[2], src[2]);\
     }\
     CML_INLINE\
-    void mat3_mul_vec3(vec3* out, mat3* m, vec3* v) {\
-        out->x = vec3_dot(&m->col[0], v);\
-        out->y = vec3_dot(&m->col[1], v);\
-        out->z = vec3_dot(&m->col[2], v);\
+    void mat3_mul_vec3(vec3 out, mat3 m, vec3 v) {\
+        out[0] = vec3_dot(m[0], v);\
+        out[1] = vec3_dot(m[1], v);\
+        out[2] = vec3_dot(m[2], v);\
     }\
     CML_INLINE\
-    void mat3_mul_mat3(mat3* out, mat3* m1, mat3* m2) {\
-        vec3 temp = INIT_VEC3(m2->col[0].x, m2->col[1].x, m2->col[2].x);\
-        out->col[0].x = vec3_dot(&m1->col[0], &temp);\
-        out->col[1].x = vec3_dot(&m1->col[1], &temp);\
-        out->col[2].x = vec3_dot(&m1->col[2], &temp);\
-        temp = INIT_VEC3(m2->col[0].y, m2->col[1].y, m2->col[2].y);\
-        out->col[0].y = vec3_dot(&m1->col[0], &temp);\
-        out->col[1].y = vec3_dot(&m1->col[1], &temp);\
-        out->col[2].y = vec3_dot(&m1->col[2], &temp);\
-        temp = INIT_VEC3(m2->col[0].z, m2->col[1].z, m2->col[2].z);\
-        out->col[0].z = vec3_dot(&m1->col[0], &temp);\
-        out->col[1].z = vec3_dot(&m1->col[1], &temp);\
-        out->col[2].z = vec3_dot(&m1->col[2], &temp);\
+    void mat3_mul_mat3(mat3 out, mat3 m1, mat3 m2) {\
+        {\
+        vec3 temp = { m2[0][0], m2[1][0], m2[2][0] };\
+        out[0][0] = vec3_dot(m1[0], temp);\
+        out[1][0] = vec3_dot(m1[1], temp);\
+        out[2][0] = vec3_dot(m1[2], temp);\
+        }\
+        {\
+        vec3 temp = { m2[0][1], m2[1][1], m2[2][1] };\
+        out[0][1] = vec3_dot(m1[0], temp);\
+        out[1][1] = vec3_dot(m1[1], temp);\
+        out[2][1] = vec3_dot(m1[2], temp);\
+        }\
+        {\
+        vec3 temp = { m2[0][2], m2[1][2], m2[2][2] };\
+        out[0][2] = vec3_dot(m1[0], temp);\
+        out[1][2] = vec3_dot(m1[1], temp);\
+        out[2][2] = vec3_dot(m1[2], temp);\
+        }\
     }\
 
 
 #define DEFINE_DEFAULT_BASIC_MAT4_BEHAVIOUR(T)\
     CML_INLINE\
-    void mat4_zero(mat4* m) {\
-        memset(&m->col[0], 0, sizeof(T) * 4);\
-        memset(&m->col[1], 0, sizeof(T) * 4);\
-        memset(&m->col[2], 0, sizeof(T) * 4);\
-        memset(&m->col[3], 0, sizeof(T) * 4);\
+    void mat4_zero(mat4 m) {\
+        memset(m[0], 0, sizeof(T) * 4);\
+        memset(m[1], 0, sizeof(T) * 4);\
+        memset(m[2], 0, sizeof(T) * 4);\
+        memset(m[3], 0, sizeof(T) * 4);\
     }\
     CML_INLINE\
-    void mat4_copy(mat4* dest, mat4* src) {\
-        vec4_copy(&dest->col[0], &src->col[0]);\
-        vec4_copy(&dest->col[1], &src->col[1]);\
-        vec4_copy(&dest->col[2], &src->col[2]);\
-        vec4_copy(&dest->col[3], &src->col[3]);\
+    void mat4_copy(mat4 dest, mat4 src) {\
+        vec4_copy(dest[0], src[0]);\
+        vec4_copy(dest[1], src[1]);\
+        vec4_copy(dest[2], src[2]);\
+        vec4_copy(dest[3], src[3]);\
     }\
     CML_INLINE\
-    void mat4_mul_vec4(vec4* out, mat4* m, vec4* v) {\
-        out->x = vec4_dot(&m->col[0], v);\
-        out->y = vec4_dot(&m->col[1], v);\
-        out->z = vec4_dot(&m->col[2], v);\
-        out->w = vec4_dot(&m->col[3], v);\
+    void mat4_mul_vec4(vec4 out, mat4 m, vec4 v) {\
+        out[0] = vec4_dot(m[0], v);\
+        out[1] = vec4_dot(m[1], v);\
+        out[2] = vec4_dot(m[2], v);\
+        out[3] = vec4_dot(m[3], v);\
     }\
     CML_INLINE\
-    void mat4_mul_mat4(mat4* out, mat4* m1, mat4* m2) {\
-        vec4 temp = INIT_VEC4(m2->col[0].x, m2->col[1].x, m2->col[2].x, m2->col[3].x);\
-        out->col[0].x = vec4_dot(&m1->col[0], &temp);\
-        out->col[1].x = vec4_dot(&m1->col[1], &temp);\
-        out->col[2].x = vec4_dot(&m1->col[2], &temp);\
-        out->col[3].x = vec4_dot(&m1->col[3], &temp);\
-        temp = INIT_VEC4(m2->col[0].y, m2->col[1].y, m2->col[2].y, m2->col[3].y);\
-        out->col[0].y = vec4_dot(&m1->col[0], &temp);\
-        out->col[1].y = vec4_dot(&m1->col[1], &temp);\
-        out->col[2].y = vec4_dot(&m1->col[2], &temp);\
-        out->col[3].y = vec4_dot(&m1->col[3], &temp);\
-        temp = INIT_VEC4(m2->col[0].z, m2->col[1].z, m2->col[2].z, m2->col[3].z);\
-        out->col[0].z = vec4_dot(&m1->col[0], &temp);\
-        out->col[1].z = vec4_dot(&m1->col[1], &temp);\
-        out->col[2].z = vec4_dot(&m1->col[2], &temp);\
-        out->col[3].z = vec4_dot(&m1->col[3], &temp);\
-        temp = INIT_VEC4(m2->col[0].w, m2->col[1].w, m2->col[2].w, m2->col[3].w);\
-        out->col[0].w = vec4_dot(&m1->col[0], &temp);\
-        out->col[1].w = vec4_dot(&m1->col[1], &temp);\
-        out->col[2].w = vec4_dot(&m1->col[2], &temp);\
-        out->col[3].w = vec4_dot(&m1->col[3], &temp);\
+    void mat4_mul_mat4(mat4 out, mat4 m1, mat4 m2) {\
+        {\
+        vec4 temp = { m2[0][0],  m2[1][0],  m2[2][0], m2[3][0] };\
+        out[0][0] = vec4_dot(m1[0], temp);\
+        out[1][0] = vec4_dot(m1[1], temp);\
+        out[2][0] = vec4_dot(m1[2], temp);\
+        out[3][0] = vec4_dot(m1[3], temp);\
+        }\
+        {\
+        vec4 temp = { m2[0][1],  m2[1][1],  m2[2][1], m2[3][1] };\
+        out[0][1] = vec4_dot(m1[0], temp);\
+        out[1][1] = vec4_dot(m1[1], temp);\
+        out[2][1] = vec4_dot(m1[2], temp);\
+        out[3][1] = vec4_dot(m1[3], temp);\
+        }\
+        {\
+        vec4 temp = { m2[0][2],  m2[1][2],  m2[2][2], m2[3][2] };\
+        out[0][2] = vec4_dot(m1[0], temp);\
+        out[1][2] = vec4_dot(m1[1], temp);\
+        out[2][2] = vec4_dot(m1[2], temp);\
+        out[3][2] = vec4_dot(m1[3], temp);\
+        }\
+        {\
+        vec4 temp = { m2[0][3],  m2[1][3],  m2[2][3], m2[3][3] };\
+        out[0][3] = vec4_dot(m1[0], temp);\
+        out[1][3] = vec4_dot(m1[1], temp);\
+        out[2][3] = vec4_dot(m1[2], temp);\
+        out[3][3] = vec4_dot(m1[3], temp);\
+        }\
     }\
 
 
@@ -310,17 +320,14 @@
     DEFINE_DEFAULT_BASIC_MAT3_BEHAVIOUR(T)\
     DEFINE_DEFAULT_BASIC_MAT4_BEHAVIOUR(T)
 
-#define INIT_VEC3i(x, y, z) (vec3i){ { x, y, z } }
 #define INIT_VEC4i(x, y, z, w) (vec4i){ { x, y, z, w } }
 DEFINE_VEC(i32, i)
 DEFINE_MAT(i32, i)
 
-#define INIT_VEC3b(x, y, z) (vec3b){ { x, y, z } }
 #define INIT_VEC4b(x, y, z, w) (vec4b){ { x, y, z, w } }
 DEFINE_VEC(u8, b)
 DEFINE_MAT(u8, b)
 
-#define INIT_VEC3(x, y, z) (vec3){ { x, y, z } }
 #define INIT_VEC4(x, y, z, w) (vec4){ { x, y, z, w } }
 DEFINE_DEFAULT_VEC(f32, f)
 DEFINE_DEFAULT_MAT(f32, f)
--- cml/src/main.c
+++ cml/src/main.c
@@ -2,157 +2,192 @@
 #include <time.h>
 #include "cml.h"
 
-void print_vec3(vec3* v) {
-    printf("%-6g %-6g %-6g\n", v->x, v->y, v->z);
+void print_vec3(vec3 v) {
+    printf("%-6g %-6g %-6g\n", v[0], v[1], v[2]);
 }
 
-void print_vec4(vec4* v) {
-    printf("%-6g %-6g %-6g %-6g\n", v->x, v->y, v->z, v->w);
+void print_vec4(vec4 v) {
+    printf("%-6g %-6g %-6g %-6g\n", v[0], v[1], v[2], v[3]);
 }
 
-void print_mat3(mat3* m) {
-    print_vec3(&m->col[0]);
-    print_vec3(&m->col[1]);
-    print_vec3(&m->col[2]);
+void print_mat3(mat3 m) {
+    print_vec3(m[0]);
+    print_vec3(m[1]);
+    print_vec3(m[2]);
 }
 
-void print_mat4(mat4* m) {
-    print_vec4(&m->col[0]);
-    print_vec4(&m->col[1]);
-    print_vec4(&m->col[2]);
-    print_vec4(&m->col[3]);
+void print_mat4(mat4 m) {
+    print_vec4(m[0]);
+    print_vec4(m[1]);
+    print_vec4(m[2]);
+    print_vec4(m[3]);
 }
 
 void test_mat3(void) {
-    vec3 v = INIT_VEC3(1, 2, 3);
-    mat3 a, b;
+    {
+        vec3 v = { 1, 2, 3 };
+        mat3 a = {
+            { 1, 0, 0 },
+            { 0, 1, 1 },
+            { 0, 1, 1 },
+        };
 
-    a.col[0] = INIT_VEC3(1, 0, 0);
-    a.col[1] = INIT_VEC3(0, 1, 1);
-    a.col[2] = INIT_VEC3(0, 1, 1);
+        vec3 ov;
 
-    printf("M:\n");
-    print_mat3(&a);
-    printf("\nV:\n");
-    print_vec3(&v);
+        printf("M:\n");
+        print_mat3(a);
+        printf("\nV:\n");
+        print_vec3(v);
 
-    mat3_mul_vec3(&v, &a, &v);
-    printf("\nM x V:\n");
-    print_vec3(&v);
+        mat3_mul_vec3(ov, a, v);
+        printf("\nM x V:\n");
+        print_vec3(ov);
+    }
 
-    a.col[0] = INIT_VEC3(10, 20, 10);
-    a.col[1] = INIT_VEC3(4, 5, 6);
-    a.col[2] = INIT_VEC3(2, 3, 5);
+    {
+        mat3 a = {
+            { 10, 20, 10 },
+            { 4, 5, 6 },
+            { 2, 3, 5 },
+        };
+        mat3 b = {
+            { 3, 2, 4 },
+            { 3, 3, 9 },
+            { 4, 4, 2 },
+        };
 
-    b.col[0] = INIT_VEC3(3, 2, 4);
-    b.col[1] = INIT_VEC3(3, 3, 9);
-    b.col[2] = INIT_VEC3(4, 4, 2);
+        mat3 om;
 
-    printf("A:\n");
-    print_mat3(&a);
-    printf("\nB:\n");
-    print_mat3(&b);
+        printf("A:\n");
+        print_mat3(a);
+        printf("\nB:\n");
+        print_mat3(b);
 
-    mat3_mul_mat3(&b, &a, &b);
-    printf("\nA x B:\n");
-    print_mat3(&b);
+        mat3_mul_mat3(om, a, b);
+        printf("\nA x B:\n");
+        print_mat3(om);
+    }
 }
 
 void test_mat4(void) {
-    vec4 v = INIT_VEC4(1, 2, 3, 4);
-    mat4 a, b;
+    {
+        vec4 v = { 1, 2, 3, 4 };
+        mat4 a = {
+            { 1, 0, 0, 1 },
+            { 0, 1, 1, 0 },
+            { 0, 1, 1, 0 },
+            { 1, 0, 0, 1 },
+        };
 
-    a.col[0] = INIT_VEC4(1, 0, 0, 1);
-    a.col[1] = INIT_VEC4(0, 1, 1, 0);
-    a.col[2] = INIT_VEC4(0, 1, 1, 0);
-    a.col[3] = INIT_VEC4(1, 0, 0, 1);
+        vec4 ov;
 
-    printf("M:\n");
-    print_mat4(&a);
-    printf("\nV:\n");
-    print_vec4(&v);
+        printf("M:\n");
+        print_mat4(a);
+        printf("\nV:\n");
+        print_vec4(v);
 
-    mat4_mul_vec4(&v, &a, &v);
-    printf("\nM x V:\n");
-    print_vec4(&v);
+        mat4_mul_vec4(ov, a, v);
+        printf("\nM x V:\n");
+        print_vec4(ov);
+    }
 
-    a.col[0] = INIT_VEC4(5, 7, 9, 10);
-    a.col[1] = INIT_VEC4(2, 3, 3, 8);
-    a.col[2] = INIT_VEC4(8, 10, 2, 3);
-    a.col[3] = INIT_VEC4(3, 3, 4, 8);
+    {
+        mat4 a = {
+            { 5, 7, 9, 10 },
+            { 2, 3, 3, 8 },
+            { 8, 10, 2, 3 },
+            { 3, 3, 4, 8 },
+        };
 
-    b.col[0] = INIT_VEC4(3, 10, 12, 18);
-    b.col[1] = INIT_VEC4(12, 1, 4, 9);
-    b.col[2] = INIT_VEC4(9, 10, 12, 2);
-    b.col[3] = INIT_VEC4(3, 12, 4, 10);
+        mat4 b = {
+            { 3, 10, 12, 18 },
+            { 12, 1, 4, 9 },
+            { 9, 10, 12, 2 },
+            { 3, 12, 4, 10 },
+        };
 
-    printf("\nA:\n");
-    print_mat4(&a);
-    printf("\nB:\n");
-    print_mat4(&b);
+        mat4 om;
 
-    printf("\nA x B:\n");
-    mat4_mul_mat4(&b, &a, &b);
-    print_mat4(&b);
+        printf("\nA:\n");
+        print_mat4(a);
+        printf("\nB:\n");
+        print_mat4(b);
+
+        printf("\nA x B:\n");
+        mat4_mul_mat4(om, a, b);
+        print_mat4(om);
+    }
 }
 
 #define PERFORMANCE_ITERATE 10000000
 
 void test_mat3_performance(void) {
-    vec3 v = INIT_VEC3(1, 2, 3), vo;
-    mat3 a, b, mo;
+    vec3 v = { 1, 2, 3 };
 
-    a.col[0] = INIT_VEC3(10, 20, 10);
-    a.col[1] = INIT_VEC3(4, 5, 6);
-    a.col[2] = INIT_VEC3(2, 3, 5);
+    mat3 a = {
+        { 10, 20, 10 },
+        { 4, 5, 6 },
+        { 2, 3, 5 },
+    };
 
-    b.col[0] = INIT_VEC3(3, 2, 4);
-    b.col[1] = INIT_VEC3(3, 3, 9);
-    b.col[2] = INIT_VEC3(4, 4, 2);
+    mat3 b = {
+       { 3, 2, 4 },
+       { 3, 3, 9 },
+       { 4, 4, 2 },
+    };
+
+    vec3 ov;
+    mat3 om;
 
     clock_t start, end;
 
     start = clock();
-    for (i32 i = 0; i < PERFORMANCE_ITERATE; ++i) {
-        mat3f_mul_vec3f(&vo, &a, &v);
+    for (u64 i = 0; i < PERFORMANCE_ITERATE; ++i) {
+        mat3f_mul_vec3f(ov, a, v);
     }
     end = clock();
     printf("iterate: 10000000 takes: %dms\n", (i32)(1000 * (end - start) / CLOCKS_PER_SEC));
 
     start = clock();
-    for (i32 i = 0; i < PERFORMANCE_ITERATE; ++i) {
-        mat3f_mul_mat3f(&mo, &a, &b);
+    for (u64 i = 0; i < PERFORMANCE_ITERATE; ++i) {
+        mat3f_mul_mat3f(om, a, b);
     }
     end = clock();
     printf("iterate: 10000000 takes: %dms\n", (i32)(1000 * (end - start) / CLOCKS_PER_SEC));
 }
 
 void test_mat4_performance(void) {
-    vec4 v = INIT_VEC4(1, 2, 3, 4), vo;
-    mat4 a, b, mo;
+    vec4 v = { 1, 2, 3, 4 };
 
-    a.col[0] = INIT_VEC4(5, 7, 9, 10);
-    a.col[1] = INIT_VEC4(2, 3, 3, 8);
-    a.col[2] = INIT_VEC4(8, 10, 2, 3);
-    a.col[3] = INIT_VEC4(3, 3, 4, 8);
+    mat4 a = {
+        { 5, 7, 9, 10 },
+        { 2, 3, 3, 8 },
+        { 8, 10, 2, 3 },
+        { 3, 3, 4, 8 },
+    };
 
-    b.col[0] = INIT_VEC4(3, 10, 12, 18);
-    b.col[1] = INIT_VEC4(12, 1, 4, 9);
-    b.col[2] = INIT_VEC4(9, 10, 12, 2);
-    b.col[3] = INIT_VEC4(3, 12, 4, 10);
+    mat4 b = {
+        { 3, 10, 12, 18 },
+        { 12, 1, 4, 9 },
+        { 9, 10, 12, 2 },
+        { 3, 12, 4, 10 },
+    };
+
+    vec4 ov;
+    mat4 om;
 
     clock_t start, end;
 
     start = clock();
-    for (i32 i = 0; i < PERFORMANCE_ITERATE; ++i) {
-        mat4f_mul_vec4f(&vo, &b, &v);
+    for (u64 i = 0; i < PERFORMANCE_ITERATE; ++i) {
+        mat4f_mul_vec4f(ov, b, v);
     }
     end = clock();
     printf("iterate: 10000000 takes: %dms\n", (i32)(1000 * (end - start) / CLOCKS_PER_SEC));
 
     start = clock();
-    for (i32 i = 0; i < PERFORMANCE_ITERATE; ++i) {
-        mat4f_mul_mat4f(&mo, &a, &b);
+    for (u64 i = 0; i < PERFORMANCE_ITERATE; ++i) {
+        mat4f_mul_mat4f(om, a, b);
     }
     end = clock();
     printf("iterate: 10000000 takes: %dms\n", (i32)(1000 * (end - start) / CLOCKS_PER_SEC));
@@ -161,8 +196,6 @@ void test_mat4_performance(void) {
 typedef int a[1];
 
 int main(void) {
-    //test_mat3();
-    //test_mat4();
     printf("[mat3 performance]\n");
     test_mat3_performance();
     printf("[mat4 performance]\n");
